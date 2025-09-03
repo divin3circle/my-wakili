682d78751129de10d27ca5d2e991c8437aa593ea
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-09-03 13:11:17 +0300

    Update app configuration with new bundle identifier, add splash screen options, and include expo-dev-client dependency

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,18 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
