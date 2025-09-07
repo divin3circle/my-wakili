2e1e254c00a472238b049f3d6495b1d00e0c9807
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-09-08 01:31:18 +0300

    chore: add reusables

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,27 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import "../global.css"
 import { Stack } from "expo-router";
+import { StatusBar } from 'expo-status-bar';
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { PortalHost } from "@rn-primitives/portal"
+import { useFonts } from 'expo-font';
+
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+
+  const [loaded] = useFonts({
+    Nunito: require('../assets/fonts/Nunito-VariableFont_wght.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+      <Stack />
+      <PortalHost />
+    </ThemeProvider>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -9,6 +10,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Button className="rounded-xl bg-rose-500">
+        <Text>Hello Sylus</Text>
+      </Button>
       <Text className="text-red-500 text-2xl font-bold">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
